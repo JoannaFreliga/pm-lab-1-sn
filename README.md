c22466828e4db5c2407f81ec34d84196c59f6772
Joanna Freliga <joanna.freliga@student.wat.edu.pl>
2020-11-23 15:46:14 +0100

    Zakończono zadanie 2.2.3

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Odpowiedź: Nie ma znaczenia, czy rezystor będzie przedd diodą, czy za nią.
 
 ## Zadanie 2-2-3:
 Pytanie:    *Czy każdy rodzaj pętli jest wymienny (zawsze można zastąpić jedną konstrukcję drugą)?*  
-Odpowiedź:  ...
+Odpowiedź: Jest wymienny, a decyzja, którą należy użyć w danym momencie, zależy głównie od kontekstu, w jakim ma być wykorzystana.
 
 ## Zadanie 2-2-4:
 Pytanie:    *Jak wpływa na działanie układu zwiększenie wartości stałej LED_T (np. do 100 ms)?*  
--- Zadania 2-2/src/main.cpp
+++ Zadania 2-2/src/main.cpp
@@ -2,15 +2,39 @@
 
 #define LED_RED 10
 
+#define M_KROPKA_T 200
+#define M_KROPKA_T 3 * M_KROPKA_T
+#define M_PAUZA_T 3 * M_KROPKA_T
+#define M_SPACJA_T 7 * M_KROPKA_T
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
   // put your setup code here, to run once:
 }
 
-void loop() {
-  pinMode(LED_RED, HIGH);
-  delay(500);
-  digitalWrite(LED_RED, LOW);
-  delay(500);
+void loop() 
+{
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KROPKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+  }
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KROPKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+  }
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KROPKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+  }
+  delay(M_SPACJA_T);
   // put your main code here, to run repeatedly:
 }
