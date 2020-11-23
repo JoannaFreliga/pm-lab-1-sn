0a1be111c1d5959186e0657092a04d3949ad4ae7
Joanna Freliga <joanna.freliga@student.wat.edu.pl>
2020-11-23 10:50:37 +0100

    Wykonano zadanie 2.2.1a

--- Zadania 2-2/src/main.cpp
+++ Zadania 2-2/src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
   // put your setup code here, to run once:
 }
 
 void loop() {
+  pinMode(LED_BUILTIN, HIGH);
+  digitalWrite(LED_BUILTIN, LOW);
   // put your main code here, to run repeatedly:
 }
